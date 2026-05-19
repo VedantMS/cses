aefa3df1f8f32d6d8fc900646837c7fa0a00e719
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-05-20 01:07:56 +0530

    Add CSES problem 1661: Subarray Sums II solution

---
+++ CSES_Problems/1661-Subarray_Sums_II/solution.cpp
@@ -0,0 +1,38 @@
+// CSES Problem 1661: Subarray Sums II
+// Link: https://cses.fi/problemset/task/1661/
+// Description: Given an array of n integers, your task is to count the number of subarrays havi...
+
+#include<iostream>
+#include<vector>
+#include<map>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n, x;
+    cin >> n >> x;
+
+    vector<long long> arr(n + 1, 0);
+
+    for(int i = 1; i <= n; i++) {
+        cin >> arr[i];
+        arr[i] += arr[i - 1];
+    }
+
+    map<long long, int> prefix_count;
+
+    long long ans = 0;
+
+    for(int i = 0; i <= n; i++) {
+        if(prefix_count.count(arr[i] - x))      ans += prefix_count[arr[i] - x];
+
+        prefix_count[arr[i]]++;
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
